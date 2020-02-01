93a1b2e48097b79135d179f36d6de63a3ad82736
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-02-01 13:45:43 +0100

    Primeros pasos

---
+++ Clase2020/src/HolaMundo.java
@@ -0,0 +1,10 @@
+/** Primera clase con pruebas básicas de sintaxis Java, variables, tipos
+ * @author andoni.eguiluz @ ingenieria.deusto.es
+ */
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		System.out.println( "¡Hola, mundo!" );
+	}	
+	
+}
