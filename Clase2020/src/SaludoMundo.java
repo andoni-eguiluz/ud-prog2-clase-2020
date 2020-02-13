adf45104f652f237887a688648bed268aeb568f0
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-02-13 21:20:35 +0100

    Aprendiendo Java

--- Clase2020/src/SaludoMundo.java
+++ Clase2020/src/SaludoMundo.java
@@ -1,1 +1,7 @@
-public class SaludoMundo { public static void main(String[] pp) { System.out.println( "Hola, querido universo" ); } }
+// Observa como los tabs y saltos de línea no le dicen nada a Java
+public class SaludoMundo {
+	public static void main(String[] pp) { 
+		System.out.println( "Hola, querido universo" );  // Saludo al mundo
+		// System.out.println( 2 / 0 );  // Provocar error de ejecución
+	} 
+}
---
+++ Clase2020/src/TiposBasicos.java
@@ -0,0 +1,41 @@
+
+public class TiposBasicos {
+	public static void main(String[] args) {
+		tipos();
+		tiposNoPrimitivos();
+	}
+	
+	static void tiposNoPrimitivos() {
+		String s = "";
+		System.out.println( "Longitud: " + s.length() );
+		int i = 5;
+		System.out.println( "Entero concatenado: " + i );  // Conversión implícita de i a String
+	}
+	
+	static void tipos() {
+		byte b = 5;  // 1 byte = 8 bits -128 a 127
+		short s = 5; // 16 bits  -32768 a 32767
+		int i = 5; // 32 bits
+		System.out.println( Integer.MAX_VALUE );
+		long l = 5; // 64 bits
+		System.out.println( Long.MAX_VALUE );
+		System.out.println( System.currentTimeMillis() );
+		float f = 2; // 32 bits con decimales
+		System.out.println( Float.MAX_VALUE );
+		float f2 = 200000000000.03f;
+		System.out.println( f2 );
+		double d = 200000000000.03;
+		System.out.println( d );
+		System.out.println( Double.MAX_VALUE );
+		System.out.println( Double.MIN_VALUE );
+		char c = 'ñ';
+		boolean bool = false; // true
+		// Trabajo con tipos primitivos
+		int i2 = 3;
+		System.out.println( i + i2 );
+		double d2 = d * f;  // Conversión implícita de float a double
+		float f3 = (float) d * f;  // Conversión explícita (no se puede implícita de double a float)
+		int d4 = (int) (i * d);  // Operación truncada
+		int d5 = (int) Math.round( i * d );  // Operación redondeada
+	}
+}
