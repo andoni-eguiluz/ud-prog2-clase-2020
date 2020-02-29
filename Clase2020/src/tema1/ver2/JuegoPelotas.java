fa78091866f5c5ea5b2ec7f911bdb7503c6f65fa
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-02-29 13:56:23 +0100

    Inicio juego de pelotas

--- Clase2020/src/tema1/ver2/JuegoPelotas.java
+++ Clase2020/src/tema1/ver2/JuegoPelotas.java
@@ -13,13 +13,13 @@ public class JuegoPelotas {
 	private static final int ANCHO_CASILLA = 200;  // Ancho de casilla en píxels. Si no va a cambiar, atributos constantes
 	private static final int ALTO_CASILLA = 150;  // Alto de casilla en píxels.
 	private static int altoTablero = 5;  // Nº de casillas. Si va a cambiar, atributos variables
-	private static int anchoCasillas = 5;  // Nº de casillas en horizontal.
+	private static int anchoTablero = 5;  // Nº de casillas en horizontal.
 	private static int numPelotas = 20;  // Nº de pelotas a meter en el tablero
 	private static VentanaGrafica vent;
 	private static GrupoPelotas grupo;
 	
 	public static void main(String[] args) {
-		vent = new VentanaGrafica( anchoCasillas * ANCHO_CASILLA, altoTablero * ALTO_CASILLA, "Juego" );
+		vent = new VentanaGrafica( anchoTablero * ANCHO_CASILLA, altoTablero * ALTO_CASILLA, "Juego" );
 		Random r = new Random();
 		grupo = new GrupoPelotas( numPelotas );
 		for (int i=0; i<numPelotas; i++) {
