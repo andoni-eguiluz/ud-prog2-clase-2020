a5103508958fb1e9ccc3836cfeebc249600400c9
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-04-09 22:55:00 +0200

    Ejemplo varargs

--- Clase2020/src/tema5/ejemplos/EjemploVarArgs.java
+++ Clase2020/src/tema5/ejemplos/EjemploVarArgs.java
@@ -15,20 +15,21 @@ public class EjemploVarArgs {
 		parsVars();
 	}
 	
-	// Ejemplo para parámetros variables. Supongamos que queremos calcular una media de números reales
 	private static void parsVars() {
+		// Ejemplo para parámetros variables. Supongamos que queremos calcular una media de números reales
 		// (1) Podríamos partir de una media de dos enteros:
-		System.out.println( media(2,5) );
+		System.out.println( media( 2, 5 ) );
 		// (2) Pero ¿y si necesitamos números reales? Java permite sobrecargar los métodos, con lo que podemos definir otro método que se llame igual
-		System.out.println( media(2.0,5.0) );
+		System.out.println( media( 2.0, 5.0 ) );
 		// (3) Y ahora necesitamos la media de tres números... pues sobrecargamos otro método más:
-		System.out.println( media(2.0,5.0,7.0) );
+		System.out.println( media( 2.0, 5.0, 7.0 ) );
 		// (4) Obviamente esto no se puede mantener indefinidamente. Y para eso Java incorporó la notación de parámetros variables.
 		// (Ver el método en su lugar de definición)
 		// Ahora se puede calcular la media de cualquier número de valores, separados por comas:
 		System.out.println( media( 2.0, 5.0, 7.0, 11.0, 28.5, 43.23 ) );
 		// Alternativamente, se puede llamar pasando un array y funciona de la misma forma:
 		System.out.println( media( new double[] { 2.0, 5.0, 7.0, 11.0, 28.5, 43.23 } ) );
+		
 		// Ejemplo con objetos
 		System.out.println( concatenar( "Concatenación de objetos", ',', "\"número 1\"", new Point(2,2), new Date(), new Double( 4.0 ) ) );
 	}
@@ -47,15 +48,6 @@ public class EjemploVarArgs {
 	private static double media( double d1, double d2, double d3) {
 		return (d1+d2+d3) / 3.0;
 	}
-	/* Nota: esto sería ambiguo
-	private static double media( double... ad ) {
-		double suma = 0;
-		for (double d : ad) {
-			suma += d;
-		}
-		return suma / (ad.length);
-	}
-	*/
 	
 	// Método (4) - varargs!  
 	// Un parámetro de la lista de parámetros puede tener los TRES PUNTOS (...)
